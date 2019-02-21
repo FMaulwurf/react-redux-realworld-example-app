6ec7aa46d94a77ddb7833757a15495cbca2c5e48
Felix Maulwurf <felix-maulwurf@hotmail.de>
2019-02-21 19:38:58 +0100

    fixed test

--- cypress/integration/article_statistics_spec.js
+++ cypress/integration/article_statistics_spec.js
@@ -3,18 +3,54 @@ import { loadavg } from "os";
 describe('Article Statistics', () => {
   beforeEach(() => {
     cy.server();
-  })
-
-  it('Should success loading shares', () => {
-    cy.route(/.*\/api\/articles\/how-to-train-your-dragon/, 'fixture:article.json').as('loadArticle');
-    cy.route(/.*\/api\/articles\/how-to-train-your-dragon\/shares/, 'fixture:shares.json').as('loadShares');
-
-    cy.visit('/article/how-to-train-your-dragon/statistics', {
+    cy.visit('/login', {
       // see https://github.com/cypress-io/cypress/issues/95#issuecomment-281273126
       onBeforeLoad(win) {
         win.fetch = null;
       }
     });
+    const registeredUser = {
+      username: 'jake',
+      email: 'ja@ke.com',
+      password: 'P4$$w0rd'
+    };
+
+    cy.route('POST', /.*\/api\/users/, {
+      user: {
+        id: Date.now(),
+        email: registeredUser.email,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        username: registeredUser.username,
+        bio: null,
+        image: null,
+        token: 'jwt'
+      }
+    }).as('postUser');
+
+    cy.get('input[placeholder="Email"]').type(registeredUser.email);
+    cy.get('input[placeholder="Password"]').type(`${registeredUser.password}{enter}`);
+
+    cy.wait(['@postUser']);
+  })
+
+  it('Should success loading shares', () => {
+    cy.route(/.*\/api\/articles\/how-to-train-your-dragon/, 'fixture:article.json').as('loadArticle');
+    cy.route(/.*\/api\/articles\/how-to-train-your-dragon\/comments/, 'fixture:comments.json').as('loadComments');
+    cy.route(/.*\/api\/articles\/how-to-train-your-dragon\/shares/, 'fixture:shares.json').as('loadShares');
+    cy.route(/.*\/api\/user/, 'fixture:user.json').as('loadUser');
+
+    cy.visit('/article/how-to-train-your-dragon', {
+      // see https://github.com/cypress-io/cypress/issues/95#issuecomment-281273126
+      onBeforeLoad(win) {
+        win.fetch = null;
+      }
+    });
+
+    cy.wait(['@loadComments', '@loadArticle', '@loadUser']);
+
+    cy.get('.btn-outline-seconday').contains('Statistics').should('exist');
+    cy.get('.btn-outline-seconday').contains('Statistics').click();
 
     cy.wait(['@loadArticle', '@loadShares']);
     cy.get('.dot').its('length').should('be', 7);
